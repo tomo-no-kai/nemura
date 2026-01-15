96f2b2a2043ff9301bba3dde445787be6568b000
tomo-no-kai <kumanote.2tu@gmail.com>
2026-01-15 14:32:51 +0900

    [fix]外部ソース画像が制御不能なため表示方式を変更

--- app/latest/[id]/page.tsx
+++ app/latest/[id]/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import React, { useState, useEffect } from 'react'
 import { useParams, useRouter } from 'next/navigation';
 
--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -53,14 +53,13 @@ export default function NewsCard({
 
       {/* 画像 */}
       <div className="w-[100px] h-full relative flex-shrink-0">
-        imageUrl ? (
+        {imageUrl && (
           <SafeImage
-          src={imageUrl}
-          alt={item.title}
-          fill
-          className="object-cover"
-        />
-        )
+            src={imageUrl}
+            alt={item.title}
+            className="w-full h-full object-cover"
+          />
+        )}
       </div>
 
       {/* テキスト */}
